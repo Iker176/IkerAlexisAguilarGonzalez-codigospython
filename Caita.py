834976967150428d669d42e9453796df9af61ba0
Iker176 <90482320+Iker176@users.noreply.github.com>
2021-09-10 17:09:11 -0500

    Add files via upload

---
+++ Caita.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(" // \ ")
+
+
+# In[7]:
+
+
+print(" //__\ ")
+
+
+# In[4]:
+
+
+print(" || ||")
+
+
+# In[2]:
+
+
+print("||__||")
+
+
+# In[13]:
+
+
+print("  // \ ")
+print(" //___\ ")
+print(" ||   ||")
+print(" ||___||")
+
+
+# In[ ]:
+
+
+
+
