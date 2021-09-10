d4f054aef3299115a5bcea5ea8f9380e62a5066c
Iker176 <90482320+Iker176@users.noreply.github.com>
2021-09-10 17:09:56 -0500

    Add files via upload

---
+++ Bienvenida.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+print("introduce tu nombre")
+nombre = input()
+print("anota tu telefono")
+telefono = input()
+print("digita tu correo")
+correo = input()
+print("Bienvenido, estos son tus datos ") 
+print(nombre)
+print(telefono)
+print(correo)
+
+
+# In[ ]:
+
+
+
+
